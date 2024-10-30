d721da34fe2facb5fa7af3fcce444a64c257693a
teamcrombie <equipo.crombie@4rsoluciones.com>
2024-10-30 11:29:57 -0300

    Add samples

--- iLoveIMG/Model/TaskParams/ResizeParams.cs
+++ iLoveIMG/Model/TaskParams/ResizeParams.cs
@@ -16,37 +16,37 @@ namespace iLoveIMG.Model.TaskParams
         }
 
         /// <summary>
-        ///     The width in pixels of the area to crop.
+        ///     Resize mode (pixels or percentage)
         /// </summary>
         [JsonProperty("resize_mode")]
         public string ResizeMode { get; set; }
 
         /// <summary>
-        ///     The width in pixels of the area to crop.
+        ///    The width in pixels of the resized image.
         /// </summary>
         [JsonProperty("pixels_width ")]
         public int PixelsWidth { get; set; }
 
         /// <summary>
-        ///     The width in pixels of the area to crop.
+        ///    The height in pixels of the resized image.
         /// </summary>
         [JsonProperty("pixels_height ")]
         public int PixelsHeight { get; set; }
 
         /// <summary>
-        ///     The width in pixels of the area to crop.
+        ///     The percentage value to resize.
         /// </summary>
         [JsonProperty("percentage")]
         public int Percentage { get; set; }
 
         /// <summary>
-        ///     The width in pixels of the area to crop.
+        ///     If set as true, resize will keep aspect ratio and images will not be distort.
         /// </summary>
         [JsonProperty("maintain_ratio ")]
         public bool MaintainRatio { get; set; }
 
         /// <summary>
-        ///     The width in pixels of the area to crop.
+        ///     Controls if the image can be gigger than the original on resize.
         /// </summary>
         [JsonProperty("no_enlarge_if_smaller  ")]
         public bool NoEnlargeIfSmaller { get; set; }
